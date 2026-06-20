0e423733475978a4bc510fd40e0d307129be4097
Raisul-0007 <raisulislamriat007@gmail.com>
2026-06-20 09:22:25 +0600

    Cart responsive done

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -8,6 +8,7 @@ import { discreament, increament, removeCart } from '../components/slice/product
 
 const Cart = () => {
      window.scrollTo(0, 0)
+     
     let dicpatch = useDispatch()
     let cartData = useSelector((state)=>state.product.cartItem)
     let handleRemove =(item)=>{
@@ -19,6 +20,14 @@ const Cart = () => {
     let handleminus = (item)=>{
         dicpatch(discreament(item))
     }
+    
+    let grandTotal = cartData.reduce((total,item)=>{
+  return total + (
+    (item.price - (item.price * item.discountPercentage / 100))
+    * item.qun
+  )
+},0)
+
   return (
     <div>
         <Container>
@@ -26,47 +35,55 @@ const Cart = () => {
                 <h1 className='dm-sons text-[36px] font-bold'>Cart</h1>
                 <p className='dm-sons text-[10px] flex gap-1 items-center text-[#6D6D60]'>Home  <MdKeyboardArrowRight />  Cart</p>
             </div>
-            <div className="flex bg-[#F5F5F3] py-[20px] pl-2">
-                    <div className="w-1/4">Product</div>
-                    <div className="w-1/4">Price</div>
-                    <div className="w-1/4">Quantity</div>
-                    <div className="w-1/4">Total</div>
-            </div>
+            {cartData.length > 0 &&
+            (<div className="flex bg-[#F5F5F3] lg:py-[20px] py-3 pl-2">
+                    <div className="w-2/5 text-center lg:text-[20px] text-sm">Product</div>
+                    <div className="w-1/5 text-center lg:text-[20px] text-sm">Price</div>
+                    <div className="w-1/5 text-center lg:text-[20px] text-sm">Quantity</div>
+                    <div className="w-1/5 text-center lg:text-[20px] text-sm">Total</div>
+            </div>)
+}
             {cartData.length > 0 ?
             (cartData.map((item)=>(
                 <div className="">
                     <div className="flex items-center pl-2 border-1 border-solid border-[#F0F0F0] py-[10px]">
-                    <div className="w-1/4 flex items-center">
+                    <div className="w-2/5  flex items-center">
                     <div onClick={()=>handleRemove(item)} className="cursor-pointer">
                         <RxCross2 />
                     </div>
                     <div className="h-15 w-15">
                         <img src={item.thumbnail} alt="" />
                     </div>
-                    <div className="pr-4">
-                        {item.title}
+                    <div className="pr-4 lg:text-[20px] text-sm">
+                        {item.title.slice(0, 15)}
                     </div>
                     </div>
-                    <div className="w-1/4">${(item.price - (item.price * item.discountPercentage / 100)).toFixed(2)}</div>
-                    <div className="w-1/4 flex">
-                    <div className=" gap-3 py-[1px] border-1 order-solid border-[#F0F0F0] flex">
-                    <button onClick={()=>handleminus(item)} className='p-1 cursor-pointer hover:bg-[#F0F0F0]'><FiMinus/></button>
-                    <p className='dm-sons text-[14px]'>{item.qun}</p>
-                    <button onClick={()=>handleplus(item)} className='p-1 cursor-pointer hover:bg-[#F0F0F0]'><FiPlus/></button>
+                    <div className="w-1/5 lg:text-[20px] text-center text-sm">${(item.price - (item.price * item.discountPercentage / 100)).toFixed(2)}</div>
+                    <div className="w-1/5  flex justify-center">
+                    <div className=" lg:gap-3 gap-1 py-[1px] border-1 order-solid border-[#F0F0F0] flex items-center">
+                    <button onClick={()=>handleminus(item)} className='p-1 cursor-pointer hover:bg-[#F0F0F0] text-sm'><FiMinus/></button>
+                    <p className='dm-sons lg:text-[14px] text-[12px]'>{item.qun}</p>
+                    <button onClick={()=>handleplus(item)} className='p-1 cursor-pointer hover:bg-[#F0F0F0] text-sm'><FiPlus/></button>
                     </div>
                     </div>
-                    <div className="w-1/4">
-                    <h3 className='dm-sons text-[20pxpx]'>{((item.price - (item.price * item.discountPercentage / 100))* item.qun).toFixed(2)}</h3>
+                    <div className="w-1/5 text-center">
+                    <h3 className='dm-sons lg:text-[20px] text-sm'>{((item.price - (item.price * item.discountPercentage / 100))* item.qun).toFixed(2)}</h3>
                     </div>
                 </div>
                 </div>
             ))):
             <h2 className="text-center dm-sons text-[36px] py-[50px]">Your Cart Is Empty</h2>
             }
-              <div className="flex">
-                    <div className="py-[5px] px-[15px]">Grand Total</div>
-                    <div className="py-[5px] px-[15px]"></div>
+            {cartData.length > 0 &&(
+              <div className=" border ml-auto lg:w-1/5 w-1/3 my-8 flex items-center rounded-2xl">
+                    <div className="py-2 w-3/5 text-center border-r-1 lg:text-xl text-[12px]  bg-[#F5F5F3] rounded-l-2xl">
+                        Grand Total :
+                    </div>
+                    <div className="lg:text-[18px] text-[10px] w-2/5 py-2 text-center">
+                        {grandTotal.toFixed(2)}
+                    </div>
                 </div>
+                )}
         </Container>
     </div>
   )
